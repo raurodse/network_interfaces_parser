c6cf50efeb19a53430c663889db8896d283f58ef
Raul Rodrigo <raurodse@gmail.com>
2013-11-05 18:06:04 +0100

    install script

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(  name = "python-netinterfaces",
+        version  = "0.1",
+        author  = "Raul Rodrigo Segura",
+        author_email = "raurodse@gmail.com",
+        license="GPL", 
+        package_dir = {'': 'src'},
+        packages = ['netinterfaces']
+     )
